e68e2b8ed1e3a5148eb2d74da52799b9cc810855
黄飞鹏 <1094521084@qq.com>
2023-04-23 17:24:30 +0800

    feat:动态设置当前标题

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -30,6 +30,15 @@ const switchHomeComponent = (type) => {
   console.log('>>>>>执行切换', type);
   isActiveComps.value = type;
 };
+/* 设置当前标题 */
+const titleMap = {
+  conversation: '会话列表',
+  contacts: '联系人',
+  me: '我的',
+};
+uni.setNavigationBarTitle({
+  title: titleMap[isActiveComps.value],
+});
 </script>
 
 <style></style>
