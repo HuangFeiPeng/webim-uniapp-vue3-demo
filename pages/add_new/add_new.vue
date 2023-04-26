082f233b6d9ba13d42d11def30e6b456abcca8c5
黄飞鹏 <1094521084@qq.com>
2023-04-26 09:56:10 +0800

    modify:添加好友改造.

--- pages/add_new/add_new.vue
+++ pages/add_new/add_new.vue
@@ -1,44 +1,13 @@
 <template>
   <view>
-    <!-- <block data-type="template" data-is="toast" data-attr="..._toast_">
-        <view class="toast_content_box">
-            <view class="toast_content" v-if="isHidefil">
-                <view class="toast_content_border"></view>
-                <view class="toast_content_icon">
-                    <image class="toast_icon_img" src="/static/images/filled2x.png"></image>
-                </view>
-                <view class="toast_content_text">{{content}}</view>
-            </view>
-    
-            <view class="toast_content_box">
-                <view class="toast_content" v-if="isHidescss">
-                    <view class="toast_content_border toast_success"></view>
-                    <view class="toast_content_icon">
-                        <image class="toast_icon_img" src="/static/images/success2x.png"></image>
-                    </view>
-                    <view class="toast_content_text">{{content}}</view>
-                </view>
-            </view>
-        </view>
-    </block> -->
-    <!-- <view class="search_input">
-        <view>
-            <input placeholder="好友名称" placeholder-style="color:#CFCFCF;line-height:20px;font-size:12px;" auto-focus bindinput="bindFriendName"></input>
-        </view>
-    </view> -->
-
-    <!-- <button bindtap="add_friend">发送申请</button> -->
-
     <view class="search" v-if="addNewState.search_btn">
       <view @tap="openSearch">
-        <icon type="search" size="13"></icon>
         <text>请输入好友用户名</text>
       </view>
     </view>
 
     <view class="search_input" v-if="addNewState.search_chats">
       <view>
-        <icon type="search" size="13"></icon>
         <input
           placeholder="请输入好友用户名"
           placeholder-style="color:#CFCFCF;line-height:20px;font-size:12px;"
@@ -47,14 +16,8 @@
           type="text"
           @confirm="onSearch"
           @input="onInput"
-          v-model="addNewState.input_code"
+          v-model.trim="addNewState.input_code"
         />
-        <icon
-          type="clear"
-          size="13"
-          @tap.stop="clearInput"
-          v-if="addNewState.show_clear"
-        ></icon>
       </view>
       <text @tap="cancel">取消</text>
     </view>
@@ -73,10 +36,14 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, computed } from 'vue';
 import { onLoad, onShow, onUnload } from '@dcloudio/uni-app';
+/* stores */
+import { useLoginStore } from '@/stores/login';
+import { useContactsStore } from '@/stores/contacts';
+/* im apis */
+import { emContacts } from '@/EaseIM/imApis';
 const WebIM = uni.WebIM;
-// let WebIM = require('../../utils/WebIM')['default'];
 const addNewState = reactive({
   friend_name: '',
   search_btn: true,
@@ -85,9 +52,11 @@ const addNewState = reactive({
   isdisable: false,
   input_code: '',
 });
-onLoad(() => {
-  let app = getApp().globalData;
+const contactsStore = useContactsStore();
+const friendList = computed(() => {
+  return contactsStore.friendList;
 });
+/* 搜索逻辑【实际并未搜索id是否存在】 */
 const openSearch = () => {
   addNewState.search_btn = false;
   addNewState.search_chats = true;
@@ -114,28 +83,30 @@ const cancel = () => {
 const onSearch = () => {
   console.log('>>>>>开始搜索');
 };
-const add_friend = () => {
-  let myName = uni.getStorageSync('myUsername');
-  if (addNewState.friend_name == '') {
-    return uni.showToast({ title: '请输入要添加的用户！' });
-  } else if (addNewState.friend_name.toLowerCase() == myName.toLowerCase()) {
-    return uni.showToast({ title: '不可添加自己为好友！' });
-  }
-  {
-    WebIM.conn.addContact(addNewState.friend_name, myName + '请求添加好友');
-    const friendList = uni.getStorageSync('member');
-    if (friendList.includes(addNewState.friend_name)) {
-      uni.showToast({
-        title: '已经是你的好友',
-      });
-      return;
-    } else {
-      uni.showToast({
-        title: '已发出好友申请',
-      });
-    }
+/* 添加好友逻辑 */
+const loginStore = useLoginStore();
 
+const { addContact } = emContacts();
+const add_friend = () => {
+  if (addNewState.friend_name === '') {
+    return uni.showToast({ title: '请输入要添加的用户！' });
+  } else if (
+    addNewState.friend_name.toLowerCase() ===
+    loginStore.loginUserBaseInfos.loginUserId
+  ) {
+    return uni.showToast({ title: '不可添加自己为好友！' });
+  } else if (friendList.value.includes(addNewState.friend_name)) {
+    uni.showToast({
+      title: '已经是你的好友',
+    });
+    return;
+  } else {
+    addContact(addNewState.friend_name, '加个好友吧！');
+    uni.showToast({
+      title: '已发出好友申请',
+    });
     addNewState.isdisable = true;
+    cancel();
   }
 };
 </script>
