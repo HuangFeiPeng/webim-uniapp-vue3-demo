b063a031059d5a84c407cfded2380c014462c2fe
黄飞鹏 <1094521084@qq.com>
2023-06-15 10:30:48 +0800

    update:sdk

--- EaseIM/index.js
+++ EaseIM/index.js
@@ -5,6 +5,7 @@ EMClient = new EaseSDK.connection({
   appKey: EM_APP_KEY,
   apiUrl: EM_API_URL,
   url: EM_WEB_SOCKET_URL,
+  deviceId: 'WebIM_UNI_APP',
 });
 uni.EMClient = EMClient;
 export { EaseSDK, EMClient };
